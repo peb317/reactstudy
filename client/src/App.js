b04effae33d1da73b74bf301cae07ef00a3c25fa
peb317 <peb317@gmail.com>
2020-08-04 20:15:18 +0900

    2020080420 add Progress bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
 import Customer from './components/Customer';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
@@ -7,61 +6,81 @@ import TableBody from '@material-ui/core/TableBody';
 import TableFooter from '@material-ui/core/TableFooter';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-
-
-const customer = [{
-  id: 1,
-  image: "https://placeimg.com/64/64/any",
-  name: "백인천1",
-  birthday: "05.13",
-  gender: "남자",
-  job: "무직",
-},{
-  id: 2,
-  image: "https://placeimg.com/64/64/any",
-  name: "백인천2",
-  birthday: "05.13",
-  gender: "남자",
-  job: "무직",
-},{
-  id: 3,
-  image: "https://placeimg.com/64/64/any",
-  name: "백인천3",
-  birthday: "05.13",
-  gender: "남자",
-  job: "무직",
-}]
+import Axios from 'axios';
+import './App.css';
 
 const styles = theme => ({
-  width: '100%',
-  height: '100vh',
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing(2),
+  },
 })
 
-function App() {
-  return (
-    <Paper>
-      <Table>
-        <TableHead fullWidth>
-          <TableRow>
-            <TableCell>번호</TableCell>
-            <TableCell>사진</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          { customer.map((c, i) => { return ( <Customer id={c.id} img={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} key={i} /> ) }) } 
-        </TableBody>
-        <TableFooter>
+class App extends Component {
 
-        </TableFooter>
-      </Table>
-    </Paper>
-  );
+  state = {
+    customers: "",
+    complated: 0,
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
+    Axios({
+      url: "http://localhost:5000/api/customers",
+      method: "get"
+    }).then( ({ data:cData }) => {
+      console.log('test > ', cData);
+        this.setState({customers:cData});
+    }).catch( error => {
+        console.log(error);
+    });
+  }
+
+  progress = () => {
+    const { complated } = this.state;
+    this.setState({ complated: complated >= 100 ? 0 : complated + 1});
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>사진</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            { this.state.customers ? this.state.customers.map((c, i) => { 
+              return (<Customer id={c.id} img={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} key={i} />) 
+            }) : 
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.progress} value={this.state.complated} />
+                </TableCell>
+              </TableRow>
+            }
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.port || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.send([{
+        id: 1,
+        image: "https://placeimg.com/64/64/any",
+        name: "백인천1",
+        birthday: "05.13",
+        gender: "남자",
+        job: "무직",
+      },{
+        id: 2,
+        image: "https://placeimg.com/64/64/any",
+        name: "백인천2",
+        birthday: "05.13",
+        gender: "남자",
+        job: "무직",
+      },{
+        id: 3,
+        image: "https://placeimg.com/64/64/any",
+        name: "백인천3",
+        birthday: "05.13",
+        gender: "남자",
+        job: "무직",
+      }
+    ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
